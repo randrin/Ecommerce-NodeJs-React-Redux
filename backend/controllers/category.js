46cf39e21cbe507f8ac85512876acbe5bee73536
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2021-01-14 17:42:10 +0100

    Update, Delete, Get Category

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -23,6 +23,44 @@ exports.categoryById = (req, res, next, id) => {
   });
 };
 
+exports.getAllCategories = (req, res) => {
+  Category.find().exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
+};
+
 exports.getCategoryById = (req, res) => {
-    return res.json(req.category);
-}
+  return res.json(req.category);
+};
+
+exports.updateCategoryById = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
+};
+
+exports.deleteCategoryById = (req, res) => {
+  const category = req.category;
+  category.remove((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      message: "Category deleted successfully!",
+    });
+  });
+};
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,13 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const { userById } = require("../controllers/user");
 const { requiredSignin, isAdmin, isAuth } = require("../controllers/auth");
 
-const { create, categoryById, getCategoryById } = require("../controllers/category");
+const {
+  create,
+  categoryById,
+  getAllCategories,
+  getCategoryById,
+  updateCategoryById,
+  deleteCategoryById,
+} = require("../controllers/category");
 
+router.get("/categories", getAllCategories);
 router.get("/:categoryId/:userId", getCategoryById);
-router.post('/create/:userId', requiredSignin, isAuth, isAdmin, create);
+router.post("/create/:userId", requiredSignin, isAuth, isAdmin, create);
+router.put(
+  "/:categoryId/:userId",
+  requiredSignin,
+  isAuth,
+  isAdmin,
+  updateCategoryById
+);
+router.delete(
+  "/:categoryId/:userId",
+  requiredSignin,
+  isAuth,
+  isAdmin,
+  deleteCategoryById
+);
 
 router.param("userId", userById);
 router.param("categoryId", categoryById);
