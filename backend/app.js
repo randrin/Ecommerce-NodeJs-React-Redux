b8daf385d083e8a103c6f2ade2e03a6e7012077b
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2021-01-14 19:52:57 +0100

    Install Cors

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const expressValidator = require('express-validator');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
+require("dotenv").config();
 
 // Import Routes
-const authRouters = require('./routes/auth');
-const userRouters = require('./routes/user');
-const categoryRouters = require('./routes/category');
-const productRouters = require('./routes/product');
+const authRouters = require("./routes/auth");
+const userRouters = require("./routes/user");
+const categoryRouters = require("./routes/category");
+const productRouters = require("./routes/product");
 
 // app
 const app = express();
 
 // Db Connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-}).then(() => console.log('MongoDb Connected'))
-.catch((error) => {
-  console.log("Error connected to mongodb");
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDb Connected"))
+  .catch((error) => {
+    console.log("Error connected to mongodb");
+  });
 
 // Middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // Routes Middleware
-app.use('/api', authRouters);
-app.use('/api', userRouters);
-app.use('/api/category', categoryRouters);
-app.use('/api/product', productRouters);
+app.use("/api", authRouters);
+app.use("/api", userRouters);
+app.use("/api/category", categoryRouters);
+app.use("/api/product", productRouters);
 
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
