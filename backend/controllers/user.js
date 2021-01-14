e0d8c33044859ed30b8625c97515fe8a13482ba1
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2021-01-14 19:49:33 +0100

    Get and Update user profile

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,3 +9,27 @@ exports.userById = (req, res, next, id) => {
     next();
   });
 };
+
+exports.getUserProfile = (req, res) => {
+  req.profile.hashed_password = undefined; // Don't pass the hashed_password to the response
+  req.profile.salt = undefined; // Don't pass the salt to the response
+  return res.json(req.profile);
+};
+
+exports.updateUserProfile = (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "You are not authorizd to perform this actions.",
+        });
+      }
+      user.hashed_password = undefined; // Don't pass the hashed_password to the response
+      user.salt = undefined; // Don't pass the salt to the response
+      res.json(user);
+    }
+  );
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const { requiredSignin, isAdmin, isAuth } = require("../controllers/auth");
 
-const { userById } = require("../controllers/user");
+const { userById, getUserProfile, updateUserProfile} = require("../controllers/user");
 
 router.get("/secret/:userId", requiredSignin, isAuth, isAdmin, (req, res) => {
   res.json({ user: req.profile });
 });
 
+router.get("/user/:userId", requiredSignin, isAuth, getUserProfile)
+router.put("/user/:userId", requiredSignin, isAuth, updateUserProfile)
 router.param("userId", userById);
 
 module.exports = router;
