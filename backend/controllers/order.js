6d39ccc7f8c8133ab84b9f0fe4ce1f18c1ec3f69
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2021-01-19 19:39:29 +0100

    Send orders to user history

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -4,7 +4,6 @@ const { errorHandler } = require("../helpers/errorHandler");
 exports.getOrders = (req, res) => {};
 
 exports.createOrder = (req, res) => {
-  console.log("CREATE ORDER: ", req.body);
   req.body.order.user = req.profile;
   const order = new Order(req.body.order);
   order.save((error, data) => {
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { userById } = require("../controllers/user");
+const { userById, addOrderToUserHistory } = require("../controllers/user");
 const { requiredSignin, isAdmin, isAuth } = require("../controllers/auth");
 const { getOrders, createOrder } = require("../controllers/order");
 
 router.get("/getOrders/:userId", requiredSignin, isAuth, getOrders);
-router.post("/create/:userId", requiredSignin, isAuth, createOrder);
+router.post("/create/:userId", requiredSignin, isAuth, addOrderToUserHistory, createOrder);
 
 router.param("userId", userById);
 
