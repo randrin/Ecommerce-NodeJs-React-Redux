3d3e1d7c2796f9c8d808b538be45fa2e5eb309e6
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2021-01-19 19:55:41 +0100

    Descrease quantity

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const { userById, addOrderToUserHistory } = require("../controllers/user");
 const { requiredSignin, isAdmin, isAuth } = require("../controllers/auth");
 const { getOrders, createOrder } = require("../controllers/order");
+const { decreaseQuantity } = require("../controllers/product");
 
 router.get("/getOrders/:userId", requiredSignin, isAuth, getOrders);
-router.post("/create/:userId", requiredSignin, isAuth, addOrderToUserHistory, createOrder);
+router.post("/create/:userId", requiredSignin, isAuth, addOrderToUserHistory, decreaseQuantity, createOrder);
 
 router.param("userId", userById);
 
